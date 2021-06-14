89ef49f6793b64edf428891c86cadf44a46cbcc5
Harper Reed <harper@nata2.org>
2021-06-13 20:54:41 -0500

    some wokr

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
+from datetime import datetime
 from discord_webhook import DiscordWebhook, DiscordEmbed
-
+import os
 from web3 import Web3
-from ens.auto import ns
+from ens import ENS
+
+
 from pprint import pprint
 
 
@@ -28,6 +31,11 @@ def grab_proposals(space, first, state):
         end
         state
         author
+        created
+
+
+        choices
+        snapshot
         
         __typename
       }
@@ -45,49 +53,113 @@ def grab_proposals(space, first, state):
   result = client.execute(query, variable_values=params)
   return result
 
+def grab_proposals(space, first, state):
+  # Select your transport with a defined url endpoint
+  transport = AIOHTTPTransport(url="https://hub.snapshot.org/graphql")
+
+  # Create a GraphQL client using the defined transport
+  client = Client(transport=transport, fetch_schema_from_transport=True)
+
+  query = gql(
+      """query Proposals($first: Int!, $skip: Int!, $state: String!, $space: String, $space_in: [String], $author_in: [String]) {
+        proposals(
+          first: $first
+        skip: $skip
+        where: {space: $space, state: $state, space_in: $space_in, author_in: $author_in}
+      ) {
+          id
+        title
+        body
+        start
+        end
+        state
+        author
+        created
+
+
+        choices
+        snapshot
+        
+        __typename
+      }
+    }"""
+  )
+  params = {
+      "first":first,
+      "skip":0,
+      "space":space,
+      "state":state,
+      "author_in":[]
+    }
+
+  # Execute the query on the transport
+  result = client.execute(query, variable_values=params)
+  return result
+
+
 def send_webhook(proposal):
-  webhook_url = ""
+  webhook_url = os.environ['DISCORD_WEBHOOK_URL']
   webhook = DiscordWebhook(url=webhook_url)
+  infura_app_id = os.environ['INFURA_APP_ID']
+  infura_url = "https://mainnet.infura.io/v3/" +infura_app_id
 
-  ens = ENS(Web3.HTTPProvider("https://mainnet.infura.io/v3/"))
+  ens = ENS(Web3.HTTPProvider(infura_url))
 
+  title = proposal['state'].upper() + ": " + proposal['title']
 
   # create embed object for webhook
-  embed = DiscordEmbed(title=proposal['title'], description=proposal['body'], color='03b2f8')
+  embed = DiscordEmbed(title=title, description=proposal['body'], color='03b2f8')
 
   author = proposal['author']
   author_url = "https://art.pizza/" + author
-  domain = ens.name(proposal['author'])
+  author_avatar_url = "https://avatar-party.web.app/a/" + author + ".png"
+  # domain = ens.name(proposal['author'])
   
-  print(domain)
-  if domain:
-    author = domain
+  # print(domain)
+  # if domain:
+  #   author = domain
+  embed.set_url("https://vote.crisis.network/#/crisisdao.eth/proposal/" + proposal['id'])
+  embed.set_author(name=author, url=author_url, icon_url=author_avatar_url)
+  embed.set_footer(text='CrisisDao Snapshot Bot')
 
-  embed.set_author(name=author, url=author_url, icon_url='https://avatars0.githubusercontent.com/u/14542790')
-  embed.set_footer(text='Embed Footer Text')
   embed.set_timestamp()
+
+  date_format = '%c'
+  
+  start_date = datetime.utcfromtimestamp(int(proposal['start'])).strftime(date_format)
+  end_date = datetime.utcfromtimestamp(int(proposal['end'])).strftime(date_format)
+  created = datetime.utcfromtimestamp(int(proposal['created'])).strftime(date_format)
+
   embed.add_embed_field(name='Status', value=proposal['state'])
-  embed.add_embed_field(name='Start', value=proposal['start'])
-  embed.add_embed_field(name='End', value=proposal['end'])
+  embed.add_embed_field(name='Snapshot', value=proposal['snapshot'])
+  embed.add_embed_field(name='Created', value=created)
+  embed.add_embed_field(name='Start', value=start_date)
+  embed.add_embed_field(name='End', value=end_date)
+
   
 
   webhook.add_embed(embed)
 
   response = webhook.execute()
 
-# result = grab_proposals("crisisdao.eth", 4, "all")
+result = grab_proposals("crisisdao.eth", 2, "all")
 
 
 
-# for p in result['proposals']:
-#   pprint(p)
-#   print("---")
-#   send_webhook(p)
+for p in result['proposals']:
+  pprint(p)
+  print("---")
+  send_webhook(p)
 
 
-w3 =Web3.HTTPProvider("https://mainnet.infura.io/v3/443ec86ea6ae445ea1499a403e14e899")
+# infura_app_id = os.environ['INFURA_APP_ID']
+# infura_url = "https://mainnet.infura.io/v3/" +infura_app_id
+
+# w3 =Web3.HTTPProvider(infura_url)
 
 
+# if (w3.isConnected()):
+#   ns = ENS.fromWeb3(w3)
 
-domain = ns.name('0xc93C7f71581DFEAAb59BEd908888dAC5689F312a')
-print(domain)
+# # domain = ns.name('0xc93C7f71581DFEAAb59BEd908888dAC5689F312a')
+# # print(domain)
